71a4d40b3e1282e1f4810b6030019e895622f9b6
Austin Bonk <bonkers@Austins-MacBook-Pro.local>
2020-03-31 10:18:00 -0500

    Adding initial py files

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+
+class AlientInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color:
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events:
+            for event in pygame.event.get():
+                if event.type == pygame.quit():
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen:
+            pygame.display.flip()
+
+if __name__ == '__main__'
+    # Make game instance, and run the game.
+    ai = AlientInvasion()
+    ai.run_game()
