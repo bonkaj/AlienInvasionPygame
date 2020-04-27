03cb3ec1d0ec02ff4722b7c7541638bd29766f6d
bonkaj <bonkers125@gmail.com>
2020-04-27 13:15:10 -0500

    Completed game!

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,6 +92,7 @@ class AlienInvasion:
             self._start_game()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
 
     def _check_keydown_events(self, event):
@@ -221,8 +222,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships left.
+            # Decrement ships left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
